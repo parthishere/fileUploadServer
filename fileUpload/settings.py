afac5731d4c46506f77865394182b6da47b9f988
parthishere <parthishere1234@gmail.com>
2023-02-01 01:33:45 +0530

    working with media file

--- fileUpload/settings.py
+++ fileUpload/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
 
 REST_FRAMEWORK = {
